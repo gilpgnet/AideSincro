9165e4cc8b197e5113b4b2e9e0de28680b5c73d0
gilpgnet <gilpgnet@gmail.com>
2019-01-01 21:36:09 -0600

    Corrige build.gradle

--- app/src/main/java/net/ramptors/asincro/MainActivity.java
+++ app/src/main/java/net/ramptors/asincro/MainActivity.java
@@ -1,9 +1,9 @@
 package net.ramptors.asincro;
 
-import android.app.*;
+import android.support.v7.app.AppCompatActivity;
 import android.os.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatActivity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
